3025a1e004761b4fb370606e722a9233a774cebe
小林　良輝 <yoshikikobayashi@yoshikiMacBook-Pro.local>
2020-01-26 14:29:13 +0900

    [Update]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '92d862bce0bff268932ce0f366a87f14d9e73502c562a971afe54a5ac39cdf80b2cbb2b654d94da8a62a2bdb2640dd016823abff08364e5613e0419034e3d82d'
